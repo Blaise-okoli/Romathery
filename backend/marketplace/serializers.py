3ae23f54b6162834a8b15d56a0eec7042d387c2f
Blaise Okoli <blaiseokoli@gmail.com>
2025-04-07 22:52:50 -0400

    Fixing image not showing issue

--- backend/marketplace/serializers.py
+++ backend/marketplace/serializers.py
@@ -4,7 +4,14 @@ from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
     vendor = serializers.StringRelatedField()
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'image', 'vendor']
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image and hasattr(obj.image, 'url'):
+            return request.build_absolute_uri(obj.image.url)
+        return None
