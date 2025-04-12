754d800df4142a6f934f237d5cb367a692428d1e
Blaise Okoli <blaiseokoli@gmail.com>
2025-04-12 08:10:39 -0400

    updating the upload to Supabase utils function

--- backend/marketplace/utils.py
+++ backend/marketplace/utils.py
@@ -11,8 +11,18 @@ supabase = create_client(supabase_url, supabase_key)
 
 def upload_image_to_supabase(file_obj, filename):
     path = f"{filename}"
-    result = supabase.storage.from_(bucket).upload(path, file_obj, {"content-type": "image/jpeg"})
+
+    # Read file contents as bytes
+    file_obj.seek(0)  # ensure we read from the beginning
+    file_bytes = file_obj.read()
+
+    result = supabase.storage.from_(bucket).upload(
+        path, 
+        file_bytes, 
+        {"content-type": "image/jpeg"}, 
+        upsert=True # allow overwrite if same file exists
+    )
     if result.get('error'):
         print("Upload error:", result['error'])
         return None
-    return f"{supabase_url}/storage/v1/object/public/{bucket}/{filename}"
+    return f"{supabase_url}/storage/v1/object/public/{bucket}/{path}"
