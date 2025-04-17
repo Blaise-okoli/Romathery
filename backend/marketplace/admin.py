fe881e1d2ec28373b666689226e73f6ca8eedb40
Blaise Okoli <blaiseokoli@gmail.com>
2025-04-16 21:30:58 -0400

    Fixing image not showing issue

--- backend/marketplace/admin.py
+++ backend/marketplace/admin.py
@@ -1,12 +1,26 @@
 from django.contrib import admin
 from .models import Vendor, Product
+from .utils import upload_image_to_supabase
+
 
 class VendorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name')
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'vendor')
     readonly_fields = ("image_url",)
+
+    def save_model(self, request, obj, form, change):
+        # Only upload if a new file was uploaded
+        if form.cleaned_data.get("image_file"):
+            public_url = upload_image_to_supabase(obj.image_file, obj.image_file.name)
+            if public_url:
+                obj.image_url = public_url
+                obj.image_file = None # clear temp file if desired
+
+        super().save_model(request, obj, form, change)
+
 
 admin.site.register(Vendor, VendorAdmin)
 admin.site.register(Product, ProductAdmin)
--- backend/marketplace/models.py
+++ backend/marketplace/models.py
@@ -17,24 +17,6 @@ class Product(models.Model):
     image_file = models.FileField(upload_to="temp/", blank=True, null=True)
     image_url = models.URLField(blank=True, null=True)
 
-    def save(self, *args, **kwargs):
-        # If a new image is uploaded via the admin
-        if self.image_file:
-            # Upload to Supabase
-            public_url = upload_image_to_supabase(self.image_file, self.image_file.name)
-            if public_url:
-                self.image_url = public_url
-
-            # Remove the temp file from local storage
-            file_path = self.image_file.path
-            if os.path.exists(file_path):
-                os.remove(file_path)
-
-            # Clear the local file field
-            self.image_file = None
-            
-
-        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
